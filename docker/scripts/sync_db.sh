16074fa57c2e8b20e822ef97c0cb0b4ac057fe47
LukaszZAtos <lukasz.zuber@atos.net>
2024-06-25 12:31:32 +0000

    v5

--- docker/scripts/sync_db.sh
+++ docker/scripts/sync_db.sh
@@ -1,16 +1,22 @@
-tabase credentials
-DEV_DB_HOST="dev_db_host"
-DEV_DB_USER="dev_db_user"
-DEV_DB_PASSWORD="dev_db_password"
-DEV_DB_NAME="prestashop"
+#!/bin/bash
 
-LOCAL_DB_HOST="localhost"
-LOCAL_DB_USER="local_db_user"
-LOCAL_DB_PASSWORD="local_db_password"
-LOCAL_DB_NAME="prestashop"
+# Database credentials
+DEV_DB_HOST=${DEV_DB_HOST}
+DEV_DB_USER=${DEV_DB_USER}
+DEV_DB_PASSWORD=${DEV_DB_PASSWORD}
+DEV_DB_NAME=${MYSQL_DATABASE}
+
+LOCAL_DB_HOST=${LOCAL_DB_HOST}
+LOCAL_DB_USER=${LOCAL_DB_USER}
+LOCAL_DB_PASSWORD=${LOCAL_DB_PASSWORD}
+LOCAL_DB_NAME=${MYSQL_DATABASE}
 
 # Define sensitive tables to be excluded from synchronization
-EXCLUDE_TABLES=("ps_orders" "ps_customers" "ps_addresses" "ps_guests" "ps_messages" "ps_logs" "ps_connections")
+EXCLUDE_TABLES=(
+  "ps_orders" "ps_order_detail" "ps_customer" "ps_address" "ps_guest" "ps_message"
+  "ps_log" "ps_connections" "ps_connections_page" "ps_connections_source"
+  "ps_customer_message" "ps_customer_thread" "ps_mail" "ps_employee"
+)
 
 # Generate exclude list for mysqldump
 EXCLUDE_LIST=""
@@ -19,16 +25,14 @@ for TABLE in "${EXCLUDE_TABLES[@]}"; do
 done
 
 # Export development database excluding sensitive data
-docker exec -i prestashop_db mysqldump -h ${DEV_DB_HOST} -u ${DEV_DB_USER} -p${DEV_DB_PASSWORD} ${DEV_DB_NAME} ${EXCLUDE_LIST} > dev_dump.sql
+docker exec -i ${PROJECT_NAME}_db mysqldump -h ${DEV_DB_HOST} -u ${DEV_DB_USER} -p${DEV_DB_PASSWORD} ${DEV_DB_NAME} ${EXCLUDE_LIST} > dev_dump.sql
 
 # Import the dump to the local database
-docker exec -i prestashop_db mysql -h ${LOCAL_DB_HOST} -u ${LOCAL_DB_USER} -p${LOCAL_DB_PASSWORD} ${LOCAL_DB_NAME} < dev_dump.sql
+docker exec -i ${PROJECT_NAME}_db mysql -h ${LOCAL_DB_HOST} -u ${LOCAL_DB_USER} -p${LOCAL_DB_PASSWORD} ${LOCAL_DB_NAME} < dev_dump.sql
 
 # Update the ps_shop_url table to match the local environment
-LOCAL_URL="http://localhost:8080"
-docker exec -i prestashop_db mysql -h ${LOCAL_DB_HOST} -u ${LOCAL_DB_USER} -p${LOCAL_DB_PASSWORD} ${LOCAL_DB_NAME} -e "UPDATE ps_shop_url SET domain='localhost', domain_ssl='localhost', physical_uri='/' WHERE id_shop_url=1;"
+LOCAL_URL=${LOCAL_URL}
+docker exec -i ${PROJECT_NAME}_db mysql -h ${LOCAL_DB_HOST} -u ${LOCAL_DB_USER} -p${LOCAL_DB_PASSWORD} ${LOCAL_DB_NAME} -e "UPDATE ps_shop_url SET domain='localhost', domain_ssl='localhost', physical_uri='/' WHERE id_shop_url=1;"
 
 # Clean up
 rm dev_dump.sql
-
-Echo "Database synchronized successfully."
