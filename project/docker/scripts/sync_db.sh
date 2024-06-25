e8e4fc0d6af6bf896404ddd2774b56cbc7973957
LukaszZAtos <lukasz.zuber@atos.net>
2024-06-25 12:13:12 +0000

    v3

--- project/docker/scripts/sync_db.sh
+++ project/docker/scripts/sync_db.sh
@@ -1,16 +1,34 @@
-#!/bin/bash
+tabase credentials
+DEV_DB_HOST="dev_db_host"
+DEV_DB_USER="dev_db_user"
+DEV_DB_PASSWORD="dev_db_password"
+DEV_DB_NAME="prestashop"
+
+LOCAL_DB_HOST="localhost"
+LOCAL_DB_USER="local_db_user"
+LOCAL_DB_PASSWORD="local_db_password"
+LOCAL_DB_NAME="prestashop"
 
 # Define sensitive tables to be excluded from synchronization
-EXCLUDE_TABLES=("orders" "customers" "addresses" "guests" "messages" "logs" "connections")
+EXCLUDE_TABLES=("ps_orders" "ps_customers" "ps_addresses" "ps_guests" "ps_messages" "ps_logs" "ps_connections")
 
 # Generate exclude list for mysqldump
 EXCLUDE_LIST=""
 for TABLE in "${EXCLUDE_TABLES[@]}"; do
-    EXCLUDE_LIST+=" --ignore-table=prestashop.${TABLE}"
+    EXCLUDE_LIST+=" --ignore-table=${DEV_DB_NAME}.${TABLE}"
 done
 
-# Export production database excluding sensitive data
-mysqldump -h production_db_host -u production_db_user -p production_db_password prestashop ${EXCLUDE_LIST} > prod_dump.sql
+# Export development database excluding sensitive data
+docker exec -i prestashop_db mysqldump -h ${DEV_DB_HOST} -u ${DEV_DB_USER} -p${DEV_DB_PASSWORD} ${DEV_DB_NAME} ${EXCLUDE_LIST} > dev_dump.sql
 
-# Import the dump to the development/local database
-mysql -h local_db_host -u local_db_user -p local_db_password prestashop < prod_dump.sql
+# Import the dump to the local database
+docker exec -i prestashop_db mysql -h ${LOCAL_DB_HOST} -u ${LOCAL_DB_USER} -p${LOCAL_DB_PASSWORD} ${LOCAL_DB_NAME} < dev_dump.sql
+
+# Update the ps_shop_url table to match the local environment
+LOCAL_URL="http://localhost:8080"
+docker exec -i prestashop_db mysql -h ${LOCAL_DB_HOST} -u ${LOCAL_DB_USER} -p${LOCAL_DB_PASSWORD} ${LOCAL_DB_NAME} -e "UPDATE ps_shop_url SET domain='localhost', domain_ssl='localhost', physical_uri='/' WHERE id_shop_url=1;"
+
+# Clean up
+rm dev_dump.sql
+
+Echo "Database synchronized successfully."
