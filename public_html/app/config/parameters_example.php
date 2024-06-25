b683c96035a7cd2cdc8aed64d62d122c7b1b7a01
LukaszZAtos <lukasz.zuber@atos.net>
2024-06-25 11:07:36 +0000

    Poprawki v2

---
+++ public_html/app/config/parameters_example.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'database_host' => 'localhost',
+    'database_port' => '3306',
+    'database_name' => 'prestashop',
+    'database_user' => 'root',
+    'database_password' => 'password',
+    'database_prefix' => 'ps_',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => 'localhost',
+    'mailer_user' => 'null',
+    'mailer_password' => 'null',
+    'secret' => 'ThisTokenIsNotSoSecretChangeIt',
+];
+?>
